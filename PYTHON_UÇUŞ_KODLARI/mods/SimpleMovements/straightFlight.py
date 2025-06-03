70bc6d3fc27290c11729174e0d3351d759ad2dd9
Berk Ülker <ulkerberk006@gmail.com>
2025-06-03 09:38:40 +0300

    straightFlight_test ve turnXDegree_test tamamen tamamlandi. main_test tamamen tamamlandi. README duzenlenecek.

--- PYTHON_UÇUŞ_KODLARI/mods/SimpleMovements/straightFlight.py
+++ PYTHON_UÇUŞ_KODLARI/mods/SimpleMovements/straightFlight.py
@@ -1,11 +1,12 @@
+import datetime
 import time as t
 from mavsdk.offboard import Attitude
 
 
 async def straightFlight(drone,
                          time,
-                         thrust,
                          altitude_enable: bool = False,
+                         thrust: float = 1.0,
                          delta: int = 5):
     try:
         async for att in drone.telemetry.attitude_euler():
@@ -13,17 +14,15 @@ async def straightFlight(drone,
             orig_yaw = att.yaw_deg
             orig_roll = att.roll_deg
             break
-
         if altitude_enable:    
             async for position in drone.telemetry.position():
                 orig_altitude = abs(position.relative_altitude_m)
                 break
         
-        pitch = -1
-
         current_time = t.time()
         t.sleep(0.1)
-        while (t.time() - current_time) < time:  
+        pitch = -1
+        while (t.time() - current_time) < time:   
             if altitude_enable:
                 async for position in drone.telemetry.position():
                     altitude = abs(position.relative_altitude_m)
@@ -39,7 +38,11 @@ async def straightFlight(drone,
             await drone.offboard.set_attitude(
                 Attitude(0, pitch, 0.0, thrust)  # Roll, Pitch, Yaw, Throttle
             )
+        await drone.offboard.set_attitude(
+            Attitude(orig_roll, orig_pitch, orig_yaw, thrust)
+        )
                 
     except Exception as e:
         print(f"Straight Flight Hatasi: {e}")
     
+   
--- PYTHON_UÇUŞ_KODLARI/mods/Tests/straightFlight_test.py
+++ PYTHON_UÇUŞ_KODLARI/mods/Tests/straightFlight_test.py
@@ -1,3 +1,5 @@
+
+import sys
 from mods.SimpleMovements import straightFlight
 
 test_sayisi = 1
@@ -5,7 +7,7 @@ success = 0
 failure = False
 
 async def straightFlight_test(drone,
-                              thrust,
+                              altitude_enable: bool = False,
                               altitude_delta: int = 5,
                               other_delta: int = 1):
 
@@ -13,39 +15,43 @@ async def straightFlight_test(drone,
 
     print("STRAIGHT FLIGHT TEST BASLADI\n")
 
-    async for position in drone.telemetry.position():
-        altitude = abs(position.relative_altitude_m)
-        break
+    if altitude_enable:
+        async for position in drone.telemetry.position():
+            altitude = abs(position.relative_altitude_m)
+            break
     async for att in drone.telemetry.attitude_euler():
-        pitch = att.pitch_deg
         yaw = att.yaw_deg
         roll = att.roll_deg
         break
 
-    await straightFlight(drone, 5, thrust)
+    await straightFlight(drone, 5)
 
-    async for position in drone.telemetry.position():
-        new_altitude = abs(position.relative_altitude_m)
-        break
+    if altitude_enable:
+        async for position in drone.telemetry.position():
+            new_altitude = abs(position.relative_altitude_m)
+            break
     async for att in drone.telemetry.attitude_euler():
-        new_pitch = att.pitch_deg
         new_yaw = att.yaw_deg
         new_roll = att.roll_deg
         break
 
-    altitude_diff = abs(new_altitude - altitude)
-    pitch_diff = abs(new_pitch - pitch)
+    if altitude_enable:
+        altitude_diff = abs(new_altitude - altitude)
     yaw_diff = abs(new_yaw - yaw)
     roll_diff = abs(new_roll - roll)
 
-    if altitude_diff > altitude_delta:
+    if altitude_enable and altitude_diff > altitude_delta:
         failure = True
-    elif pitch_diff > other_delta or yaw_diff > other_delta or roll_diff > other_delta:
+    elif yaw_diff > other_delta or roll_diff > other_delta:
         failure = True
     else:
         print("straigthFlight_test basarili.\n")
+        success += 1
 
     if failure:
-        print(f"straigthFlight_test basarisiz. (Irtifa farki: {altitude_diff:.2f})\n(Yon farklari -> pitch: {pitch_diff:.2f} yaw: {yaw_diff:.2f} roll: {roll_diff:.2f})\n")
+        print("straigthFlight_test basarisiz.")
+        if altitude_enable:
+            print(f"(Irtifa farki: {altitude_diff:.2f})")
+        print(f"(Yon farklari -> yaw: {yaw_diff:.2f} roll: {roll_diff:.2f})\n")
 
     print(f"STRAIGHT FLIGHT TEST SONLANDI. {success}/{test_sayisi}\n")
